25110318abefdfd7ec0c5fcc2f1a52221b6cac14
d_etteiu8383 <ysdr83@gmail.com>
2022-12-22 04:47:33 +0900

    add homebrew path

--- scripts/setup-homebrew.bash
+++ scripts/setup-homebrew.bash
@@ -10,6 +10,8 @@ if type brew >/dev/null; then
 else
     echo "Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> "$HOME/.profile"
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 
 echo "Updating Homebrew..."
