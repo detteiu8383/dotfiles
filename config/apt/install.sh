5b0aca8f2890f4cfcf767efb16fff978321c5519
detteiu8383 <ysdr83@gmail.com>
2022-12-27 00:19:09 +0000

    update

--- config/apt/install.sh
+++ config/apt/install.sh
@@ -1,28 +1,13 @@
 #!/bin/sh -e
 ubuntu_version="$(lsb_release -r | awk '{print $2 * 100}')"
 
-sudo add-apt-repository -y ppa:git-core/ppa
-sudo apt update
-sudo apt upgrade -y
-sudo apt install -y \
-  bat \
+sudo -E add-apt-repository -y ppa:git-core/ppa
+apt update
+apt upgrade -y
+apt install -y \
   build-essential \
-  direnv \
-  fzf \
-  git \
-  git-lfs \
-  gpg \
-  jq \
-  libssl-dev \
-  neovim \
-  python3 \
-  python3-pip \
-  python3-pynvim \
-  shellcheck \
-  tmux \
-  unzip \
-  wget \
-  zip \
-  zsh
+  curl \
+  file \
+  git
 
 curl -fsSL 'https://download.docker.com/linux/ubuntu/gpg' | apt-key add -
