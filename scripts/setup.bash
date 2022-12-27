1e4b017affdc8d03bd675009eac681caf2803f54
detteiu8383 <ysdr83@gmail.com>
2022-12-27 11:20:34 +0900

    update

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -7,3 +7,7 @@ source "$(dirname "$0")/common.bash"
 /bin/bash "$CUR_DIR/setup-homebrew.bash"
 /bin/bash "$CUR_DIR/setup-links.bash"
 /bin/bash "$CUR_DIR/setup-zinit.bash"
+
+# set default shell
+which zsh | sudo tee -a /etc/shells
+chsh -s `which zsh`
