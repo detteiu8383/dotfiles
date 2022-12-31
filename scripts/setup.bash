26cc9718e5cd1a84ced1bbdebadaac3b16e714fb
detteiu8383 <ysdr83@gmail.com>
2022-12-31 16:19:54 +0000

    :wrench: automatically install packer.nvim in plugins.lua

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -5,7 +5,6 @@ source "$(dirname "$0")/common.bash"
 
 /bin/bash "$CUR_DIR/setup-apt.bash"
 /bin/bash "$CUR_DIR/setup-homebrew.bash"
-/bin/bash "$CUR_DIR/setup-nvim.bash"
 /bin/bash "$CUR_DIR/setup-links.bash"
 /bin/bash "$CUR_DIR/setup-zinit.bash"
 
