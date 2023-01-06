5e9ef8ac8206e1af7d4c83c084a5da45d902f439
detteiu8383 <>
2023-01-06 04:00:20 +0000

    :sparkles: enable copy from wsl to windows

--- config/nvim/lua/settings/_options.lua
+++ config/nvim/lua/settings/_options.lua
@@ -40,3 +40,18 @@ o.wildoptions = "pum"
 o.winblend = 0
 o.wrap = false
 o.writebackup = false
+
+if vim.fn.executable("win32yank.exe") then
+  vim.g.clipboard = {
+    name = "myClipboard",
+    copy = {
+      ["+"] = { "win32yank.exe", "-i" },
+      ["*"] = { "win32yank.exe", "-i" },
+    },
+    paste = {
+      ["+"] = { "win32yank.exe", "-o" },
+      ["*"] = { "win32yank.exe", "-o" },
+    },
+    cache_enabled = true,
+  }
+end
