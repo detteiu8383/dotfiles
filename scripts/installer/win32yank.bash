244bfcb5924a12c9a36727411d206d799872b905
detteiu8383 <ysdr83@gmail.com>
2023-01-17 05:49:49 +0000

    :bug: change installer place

--- scripts/installer/win32yank.bash
+++ scripts/installer/win32yank.bash
@@ -2,7 +2,7 @@
 
 if ! (type "win32yank.exe" > /dev/null 2>&1); then
   echo "win32yank.exe is not installed. Install win32yank.exe..."
-  curl -sLo/tmp/win32yank.zip https://github.com/equalsraf/win32yank/releases/download/v0.0.4/win32yank-x64.zip
+  curl -sLo /tmp/win32yank.zip https://github.com/equalsraf/win32yank/releases/download/v0.0.4/win32yank-x64.zip
   unzip -p /tmp/win32yank.zip win32yank.exe > /tmp/win32yank.exe
   chmod +x /tmp/win32yank.exe
   sudo mv /tmp/win32yank.exe /usr/local/bin/
