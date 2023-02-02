1f0ba2e30bd41bfe639c67815ff5ccd3389ff024
detteiu8383 <ysdr83@gmail.com>
2023-02-02 08:06:04 +0000

    :art: make dashboard look good

--- config/nvim/lua/plugins/alpha.lua
+++ config/nvim/lua/plugins/alpha.lua
@@ -1,18 +1,36 @@
 return function()
   local alpha = require("alpha")
-  local startify_config = require("alpha.themes.startify").config
+  local dashboard = require("alpha.themes.dashboard")
+  dashboard.section.header.val = {
+    "             .,-:;//;:=,                ",
+    "         . :H@@@MM@M#H/.,+%;,           ",
+    "      ,/X+ +M@@M@MM%=,-%HMMM@X/,        ",
+    "     -+@MM; $M@@MH+-,;XMMMM@MMMM@+-     ",
+    "    ;@M@@M- XM@X;. -+XXXXXHHH@M@M#@/.   ",
+    "  ,%MM@@MH ,@%=            .---=-=:=,.  ",
+    "  -@#@@@MX .,              -%HX$$%%%+;  ",
+    " =-./@M@M$                  .;@MMMM@MM: ",
+    " X@/ -$MM/                    .+MM@@@M$ ",
+    ",@M@H: :@:                    . -X#@@@@-",
+    ",@@@MMX, .                    /H- ;@M@M=",
+    ".H@@@@M@+,                    %MM+..%#$.",
+    " /MMMM@MMH/.                  XM@MH; -; ",
+    "  /%+%$XHH@$=              , .H@@@@MX,  ",
+    "   .=--------.           -%H.,@@@@@MX,  ",
+    "   .%MM@@@HHHXX$$$%+- .:$MMX -M@@MM%.   ",
+    "     =XMMM@MM@MM#H;,-+HMM@M+ /MMMX=     ",
+    "       =%@M@M#@$-.=$@MM@@@M; %M%=       ",
+    "         ,:+$+-,/H#MMMMMMM@- -,         ",
+    "               =++%%%%+/:-.             ",
+  }
 
-  -- local header = {
-  --   type = "text",
-  --   val = "test",
-  --   opts = {
-  --     hl = "Type",
-  --     shrink_margin = false,
-  --   }
-  -- }
+  dashboard.section.buttons.val = {
+    dashboard.button("e", "  New File    ", ":enew<CR>"),
+    dashboard.button("f", "  Find File   ", ":Telescope find_files<CR>"),
+    dashboard.button("t", "  Find Text   ", ":Telescope live_grep<CR>"),
+    dashboard.button("n", "  Open Neotree", ":Neotree show<CR>"),
+    dashboard.button("q", "  Quit        ", ":qa<CR>"),
+  }
 
-  -- -- set header
-  -- startify_config.layout[2] = header
-
-  alpha.setup(startify_config)
+  alpha.setup(dashboard.opts)
 end
